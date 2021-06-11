8a0af40c486008cb1a9c61b1f2d48a8adf1d153d
Emanuel-Bravo <79419288+Emanuel-Bravo@users.noreply.github.com>
2021-06-11 15:14:13 -0300

    First Projects and lists of links
    Added my first two projects from Frontend Mentor, a file for MDN future projects and started creating the Index of the site

---
+++ projects/frontendMentor/Easybank/app/js/script.js
@@ -0,0 +1,27 @@
+const btnHamburguer = document.querySelector('#btnHamburguer');
+const body =document.querySelector('body');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems =document.querySelectorAll('.has-fade');
+
+btnHamburguer.addEventListener('click', function(){
+    console.log('teste do botão');
+
+    if(header.classList.contains('open')){ // close hamburguer menu
+        body.classList.remove('noscroll')
+        header.classList.remove('open'); // remove open
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in'); // remove fade-in
+            element.classList.add('fade-out'); // add fade-out
+        });
+        
+
+    } else {
+        body.classList.add('noscroll')
+        header.classList.add('open'); // add open
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-out'); // remove fadeout
+            element.classList.add('fade-in'); // add fade-in
+        });
+    }
+});
