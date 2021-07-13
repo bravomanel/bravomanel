3ef97c2bedbeee0bc71014431775533511bc6077
Emanuel-Bravo <emanuelbravo0706@gmail.com>
2021-07-13 16:13:02 -0300

    Add basic UI for Jokenpo

--- projects/the_odin_project/jokenpo/script.js
+++ projects/the_odin_project/jokenpo/script.js
@@ -3,12 +3,6 @@ let r = "Rock";
 let p = "Paper";
 let s = "Scissors";
 
-//Score 
-userScoreHolder = document.getElementById('userScoreHolder');
-pcScoreHolder = document.getElementById('pcScoreHolder');
-userScore = 0;
-pcScore = 0;
-
 // Messages
 
 let won = "You Won!!"
@@ -21,28 +15,98 @@ let SxP = "Scissors cuts Paper";
 let tie = "You tied";
 
 let outro = "please write again"
+let standard = "Please make your choice, who gets 5 points first Win!"
 
+let place = document.getElementById('message')
+let text = document.createElement('p');
+text.textContent = standard;
+place.appendChild(text);
+
+
+function displayResult(message) {
+    text.textContent = message;
+    place.appendChild(text);
+}
+
+    // Final Result
+
+let finalWon = "You Won the Game, congratulations!!";
+let finalLose = "The Machine Won, try harder!";
+
+
+function finalCheck () {
+    if (userScore < 5 && pcScore < 5) {
+        return;
+    } else if (userScore == 5) {
+        finalResult(finalWon);
+    } else if (pcScore == 5) {
+        finalResult(finalLose);
+    }
+
+    reset();
+}
+
+    //Final Result
+    let finalResultHolder = document.getElementById('finalResult');
+    let paragraph = document.createElement('p')
+
+function finalResult(text) {
+    paragraph.textContent = text;
+    finalResultHolder.appendChild(paragraph);
+}
+
+    //Reset
+
+    function reset() {
+        userScore = 0;
+        pcScore = 0;
+        score();
+    }
+
+    //Score 
+
+let userScore = 0;
+let pcScore = 0;
+
+let userScoreHolder = document.getElementById("userScoreHolder");
+let pcScoreHolder = document.getElementById("pcScoreHolder");
+
+let h3PC = document.createElement('h3');
+let h3User = document.createElement('h3');
+
+
+function score () {
+    h3PC.textContent = pcScore;
+    h3User.textContent = userScore;
+
+    pcScoreHolder.appendChild(h3PC);
+    userScoreHolder.appendChild(h3User);
+}
+score();
 
     // Computer Play
+
 function computerPlay () {
     /* Math random logic: 
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min)) + min;
     */
-   
+
     let random =  Math.floor(Math.random() * 3);
 
-    if (r === 0) {
+    if (random === 0) {
         return r;
     } else if (random === 1){ 
         return p;
-    } else {
+    } else if (random === 2){
         return s;
     }
 }
 
+
     // The Battle 
+
 function battle(user, pc) {
     if (user == pc) {
         return tie;
@@ -56,6 +120,7 @@ function battle(user, pc) {
 }
 
     // The Winner
+    
 function winner(user, pc) {
     if (user == r && pc == s || user == p && pc == r || user == s && pc == p) {
         userScore++;
@@ -68,13 +133,6 @@ function winner(user, pc) {
     }
 }
 
-    // Reset
-function reset() {
-    userScore = 0;
-    pcScore = 0;
-
-}
-
  // The Game
 
 function play() {
@@ -83,12 +141,11 @@ function play() {
 
     let message = `Computer played ${pc}, ${battle(user, pc)}, ${winner(user, pc)}`;
 
-    userScoreHolder.appendChild(userScore);
-    pcScoreHolder.appendChild(pcScore);
+    score();
 
-    alert(message);
-    console.log(userScore, pcScore);
-    console.log(message);
+    finalCheck(message);
+
+    displayResult(message);
 }
 
 
