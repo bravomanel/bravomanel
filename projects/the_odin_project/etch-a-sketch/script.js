3dd183c3db4cb137224666306cddb2901aa769b9
Emanuel_Bravo <emanuelbravo0706@gmail.com>
2021-07-17 19:22:55 -0300

    Add Etch a Sketch Functionality

--- projects/the_odin_project/etch-a-sketch/script.js
+++ projects/the_odin_project/etch-a-sketch/script.js
@@ -1,50 +1,50 @@
 let slider = document.getElementById("gridSizeSlider");
 let output = document.getElementById("slideShow")
 let mainGrid = document.getElementById("mainGrid");
-
-slider.oninput = function() {
-    output.innerHTML = this.value;
-  }
-
+let resetBtn = document.getElementById("resetBtn");
 let gridSize = 16;
 
 
-    
-    
-for (let i = 0; i < gridSize; i++) {
-    let gridRow = document.createElement("div");
-    gridRow.classList.add(`row${i+1}`, `gridRows`)
-    for (let j = 0; j < gridSize; j++) {
-        let gridDiv = document.createElement("div");
-        gridDiv.classList.add(`divPixel`)
-        gridDiv.id = `pixel${i}x${j}`;
-        gridDiv.addEventListener("mouseover", function() {
-            let pixel = document.getElementById(`pixel${i}x${j}`);
-            pixel.style.backgroundColor = "red";
-        });
-        gridRow.appendChild(gridDiv);
+
+
+function createArray(size) {
+    for (let i = 0; i < size; i++) {
+        let gridRow = document.createElement("div");
+        gridRow.classList.add(`gridRows`)
+
+        for (let j = 0; j < size; j++) {
+            let gridDiv = document.createElement("div");
+            gridDiv.classList.add(`divPixel`)
+            gridDiv.id = `pixel${i}x${j}`;
+            gridDiv.addEventListener("mouseover", function() {
+                let pixel = document.getElementById(`pixel${i}x${j}`);
+                pixel.style.backgroundColor = "red";
+            });
+            gridRow.appendChild(gridDiv);
+        }
+        mainGrid.appendChild(gridRow);
     }
-    mainGrid.appendChild(gridRow);
 }
 
+function painting() {
 
+}
+    
+function removeAllChildNodes() {
+    while (mainGrid.firstChild) {
+        mainGrid.removeChild(mainGrid.firstChild);
+    }
+}
 
-// let mainGrid = document.getElementById("mainGrid");
+resetBtn.addEventListener("click", function () {
+    removeAllChildNodes();
+    createArray(slider.value);
+});
 
-// for (let i = 0; i < gridSize; i++) {
-//     let gridRow = document.createElement("div");
-//     gridRow.classList.add(`row${i+1}`, `gridRows`)
-//     for (let j = 0; j < gridSize; j++) {
-//         let gridDiv = document.createElement("div");
-//         gridDiv.classList.add(`divPixel`)
-//         gridDiv.id = `pixel${i}x${j}`;
+createArray(gridSize);
 
-//         gridDiv.addEventListener("mouseover", function() {
-//             let pixel = document.getElementById(`pixel${i}x${j}`);
-//             pixel.style.backgroundColor = "red";
-//         });
-
-//         gridRow.appendChild(gridDiv);
-//     }
-//     mainGrid.appendChild(gridRow);
-// }
+slider.oninput = function () {
+    removeAllChildNodes();
+    output.innerHTML = this.value;
+    createArray(this.value);
+}
