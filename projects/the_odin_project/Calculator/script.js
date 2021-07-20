0cfeca499eb0bce2c02c050181dcd7c55c1cb358
Emanuel_Bravo <emanuelbravo0706@gmail.com>
2021-07-20 17:40:38 -0300

    Start calculator TOP project

---
+++ projects/the_odin_project/Calculator/script.js
@@ -0,0 +1,22 @@
+function sum (a, b) {
+    return a + b;
+}
+function subtract (a, b) {
+    return a - b;
+}
+function multiply (a, b) {
+    return a * b;
+}
+function divide (a, b) {
+    return a / b;
+}
+function operate(operator, a, b) {
+    operator(a,b);
+}
+
+// let container = document.querySelector('.digitsContainer')
+// for (let digit of board) {
+//     let itsId = digit.id;
+//     digit.style.gridArea = itsId;
+// }
+
