fea92b9e0888754d6ed74878e952b6d13fe43bb1
Emanuel Bravo <emanuelbravo0706@gmail.com>
2023-08-07 12:06:02 -0300

    change age automatically and favicon

--- script.js
+++ script.js
@@ -3,15 +3,32 @@ const navMobile = document.getElementById('navMobile');
 const aboutmeMobileMenuLink = document.getElementById('aboutmeMobileMenuLink');
 const projectsMobileMenuLink = document.getElementById('projectsMobileMenuLink');
 const contactMobileMenuLink = document.getElementById('contactMobileMenuLink');
+const ageline = document.getElementById('ageline');
 
 const clickButtonHandler = () => {
    mobileMenuButton.classList.toggle("change");
    navMobile.classList.toggle("navMenuTransition");
-
 }
 
-mobileMenuButton.addEventListener('click', clickButtonHandler)
+const age = () => {
+   const date = new Date();
+   const year = date.getFullYear();
+   const month = date.getMonth();
+   const day = date.getDate();
+   let age;
+   if (month < 5) {
+      age = year - 2001;
+   } else if (month === 5 && day < 7) {
+      age = year - 2001;
+   } else {
+      age = year - 2000;
+   }
+   ageline.innerHTML = age + " Years Old from Rio de Janeiro";
+}
 
+age();
+
+mobileMenuButton.addEventListener('click', clickButtonHandler)
 aboutmeMobileMenuLink.addEventListener('click', clickButtonHandler)
 projectsMobileMenuLink.addEventListener('click', clickButtonHandler)
 contactMobileMenuLink.addEventListener('click', clickButtonHandler)
